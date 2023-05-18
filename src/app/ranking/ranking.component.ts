07068342b13ca5f676b42ffad2b19d95dc942420
marcsien <marcsien@gmail.com>
2023-05-18 07:20:18 +0200

    move best quotes ranking to component, @Input()

---
+++ src/app/ranking/ranking.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Quotation } from '../models/quotiation';
+
+@Component({
+  selector: 'app-ranking',
+  templateUrl: './ranking.component.html',
+  styleUrls: ['./ranking.component.css']
+})
+export class RankingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input()
+  quotes: Quotation[];
+
+}
